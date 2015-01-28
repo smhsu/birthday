8d551f12fa1fc9bddf244258f857dcaef3e72cfb
smhsu <shoe.hsuman@gmail.com>
2015-01-28 10:11:56 -0600

    Added timer

---
+++ timer.js
@@ -0,0 +1,27 @@
+var interval = null;
+var time = 0;
+var record = 9999;
+
+function tick() {
+	time += 1;
+	document.getElementById("timer").innerHTML = ("Current time: " + time/100 + "s");
+}
+
+function startTimer() {
+	time = 0;
+	document.getElementById("timer").innerHTML = ("00.00s");
+	interval = setInterval(function() {tick()}, 10);
+}
+
+function stopTimer() {
+	if (interval)
+		clearInterval(interval);
+}
+
+function updateRecord() {
+	if (time < record) {
+		record = time;
+		document.getElementById("record").innerHTML = ("Your record: " + time/100 + "s");
+		document.getElementById("recordnote").innerHTML = ("New time record!!!");
+	}
+}
