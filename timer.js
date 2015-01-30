e511f50b5c225204e6c88b4856015003ff3be299
smhsu <shoe.hsuman@gmail.com>
2015-01-30 09:31:08 -0600

    Now support arbitrary records

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 var interval = null;
 var time = 0;
-var record = 9999;
+var record = -1;
 
 function tick() {
 	time += 1;
@@ -9,7 +9,7 @@ function tick() {
 
 function startTimer() {
 	time = 0;
-	document.getElementById("timer").innerHTML = ("00.00s");
+	document.getElementById("timer").innerHTML = ("0.00s");
 	interval = setInterval(function() {tick()}, 10);
 }
 
@@ -19,7 +19,7 @@ function stopTimer() {
 }
 
 function updateRecord() {
-	if (time < record) {
+	if (time < record || record == -1) {
 		record = time;
 		document.getElementById("record").innerHTML = ("Your record: " + time/100 + "s");
 		document.getElementById("recordnote").innerHTML = ("New time record!!!");
